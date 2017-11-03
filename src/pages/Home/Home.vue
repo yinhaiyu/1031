122ac9b4f182f14d66c93f1f2b1e99d13b4f73bd
yinhaiyu <810583010@qq.com>
2017-11-03 19:34:42 +0800

    change

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,33 +1,23 @@
 <template>
 	<div>
 		<index-header />
-<<<<<<< HEAD
 		<index-swiper />
 		<index-classifySwiper />
-=======
 		<index-hotList />
->>>>>>> f63f97244dba9b79c15972d723ecad82cb43b5b4
 	</div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import header from "./Header.vue";
 import swiper from "./Swiper.vue";
 import classifySwiper from "./ClassifySwiper.vue";
-export default {
-  components:{
-  	"index-header":header,
-  	"index-swiper":swiper,
-  	"index-classifySwiper":classifySwiper
-=======
-import header from "./header.vue";
 import hotList from "./hotList.vue";
 export default {
   components:{
   	"index-header":header,
+  	"index-swiper":swiper,
+  	"index-classifySwiper":classifySwiper,
   	"index-hotList":hotList
->>>>>>> f63f97244dba9b79c15972d723ecad82cb43b5b4
   }
 }
 </script>
