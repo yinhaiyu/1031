f63f97244dba9b79c15972d723ecad82cb43b5b4
wufan <1540984574@qq.com>
2017-11-03 16:19:03 +0800

    change

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,14 +1,17 @@
 <template>
 	<div>
 		<index-header />
+		<index-hotList />
 	</div>
 </template>
 
 <script>
 import header from "./header.vue";
+import hotList from "./hotList.vue";
 export default {
   components:{
-  	"index-header":header
+  	"index-header":header,
+  	"index-hotList":hotList
   }
 }
 </script>
