ce90f9a123738e84c21a8608dee6f1b132b79304
123 <1010368099@qq.com>
2017-11-03 19:52:34 +0800

    change

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,14 +1,17 @@
 <template>
 	<div>
 		<index-header />
+		<index-gotoList />
 	</div>
 </template>
 
 <script>
 import header from "./header.vue";
+import gotoList from "./gotoList.vue";
 export default {
   components:{
-  	"index-header":header
+  	"index-header":header,
+	"index-gotoList":gotoList
   }
 }
 </script>
